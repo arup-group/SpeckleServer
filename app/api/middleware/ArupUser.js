cc46e39e6c6f2972faade38afd3a72a91e86e3a1
Peter Grainger <peter.grainger@arup.com>
2020-12-09 14:29:43 +0000

    [SPEC-176] avoid adding user to request more than once

--- app/api/middleware/ArupUser.js
+++ app/api/middleware/ArupUser.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
-  if(req.headers.authorization) {
+  if(!req.arupUser && req.headers.authorization) {
     const auth = jwt.decode(req.headers.authorization.replace('JWT ', ''))
     if(auth && auth.email && auth.email.includes('@arup.com')) {
         req.arupUser = auth
