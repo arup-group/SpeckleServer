1d17460faa5f32c65afb7f3d16780f8f3c509e9f
Peter Grainger <peter.grainger@arup.com>
2020-12-14 21:54:49 +0000

    [SPEC-182] Add matomo tracing

--- app/telemetry/matomo.js
+++ app/telemetry/matomo.js
@@ -1,27 +1,29 @@
 const MatomoTracker = require('matomo-tracker')
-const logger = require( '../../config/logger' )
-
-
-
 
 module.exports = (app) => {
-  const matomo = new MatomoTracker(1, 'https://oasyssoftware.matomo.cloud/matomo.php')
-  app.use(( req, res, next ) => {
-    logger.info(req.user)
-    const arupUser = req.user && req.user.email && req.user.email.includes('@arup.com')
-    if(arupUser) {
-      matomo.track({
-        url: req.baseUrl + req.url,
-        action_name: 'API call',
-        ua: req.header('User-Agent'),
-        lang: req.header('Accept-Language'),
-        cvar: JSON.stringify({
-          '1': ['API version', 'v1'],
-          '2': ['HTTP method', req.method]
-        }),
-        uid: req.user.email
-      })
-    }
-    next();
-  })
+  const matomoUrl = process.env.MATOMO_URL
+  if(matomoUrl) {
+    // Site "2" is speckle
+    const matomo = new MatomoTracker(2, matomoUrl)
+    app.use(( req, res, next ) => {
+      if(req.arupUser) {
+        matomo.track({
+          // This retrieves the full url including any ports
+          url: req.protocol + '://' + req.get('host') + req.originalUrl,
+          action_name: 'API call',
+          ua: req.headers['user-agent'],
+          // Auth0 ID
+          _id: req.arupUser._id,
+          lang: req.header('accept-language'),
+          _cvar: JSON.stringify({
+            '1': ['API version', 'v1'],
+            '2': ['HTTP method', req.method]
+          }),
+          // Auth0 email address
+          uid: req.arupUser.email
+        })
+      }
+      next();
+    })
+  }
 }
