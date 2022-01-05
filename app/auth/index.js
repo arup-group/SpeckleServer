0cad1ac6038029d208f7a74e6b66629f406beb9a
Jenessa Man <jenessa.man@arup.com>
2022-01-05 11:45:16 -0500

    Add samesite attribute

--- app/auth/index.js
+++ app/auth/index.js
@@ -33,7 +33,9 @@ module.exports = function ( app ) {
     resave: false,
     saveUninitialized: true,
     cookie: {
-      maxAge: 60000 * 3
+      maxAge: 60000 * 3,
+      sameSite: 'none',
+      secure: true,
     },
     expires: new Date( Date.now( ) + 60000 * 3 ) // three minute expiration (not needed)
   } )
