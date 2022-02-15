a6e3ad7b544a89205bb4aa105fad1a46c4b7c16c
Jenessa Man <jenessa.man@arup.com>
2022-02-15 15:48:12 -0500

    Clean up object edit permissions

--- app/api/objects/ObjectPut.js
+++ app/api/objects/ObjectPut.js
@@ -1,6 +1,8 @@
 const winston = require( '../../../config/logger' )
+const mongoose = require( 'mongoose' )
 
 const SpeckleObject = require( '../../../models/SpeckleObject' )
+const DataStream = require( '../../../models/DataStream' )
 const PermissionCheck = require( '../middleware/PermissionCheck' )
 
 module.exports = ( req, res ) => {
@@ -8,15 +10,22 @@ module.exports = ( req, res ) => {
     res.status( 400 )
     return res.send( { success: false, message: 'Malformed request.' } )
   }
-  SpeckleObject.findOne( { _id: req.params.objectId } )
-    .then( result => PermissionCheck( req.user, 'write', result, Object.keys( req.body ) ) )
-    .then( result => result.set( req.body ).save() )
-    .then( () => {
-      res.send( { success: true, message: 'Object updated.' } )
-    } )
-    .catch( err => {
-      winston.error( err )
-      res.status( 400 )
-      return res.send( { success: false, message: err.toString() } )
-    } )
+
+  // derive permissions for editing an object from the permissions of the associated (containing) streams
+  // ie. if you can write to the stream, you can update the object contained in the stream
+  DataStream.find( { objects: mongoose.Types.ObjectId( req.params.objectId ) }, 'canWrite canRead streamId owner' )
+    .then( streams => Promise.all( streams.map( s => PermissionCheck( req.user, 'write', s ) ) )
+      .then( () => {
+        return SpeckleObject.findOne( { _id: req.params.objectId } )
+      } )
+      .then( result => result.set( req.body ).save() )
+      .then( () => {
+        res.send( { success: true, message: 'Object updated.' } )
+      } )
+      .catch( err => {
+        winston.error( err )
+        res.status( 400 )
+        return res.send( { success: false, message: err.toString() } )
+      } )
+    )
 }
